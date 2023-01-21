8f0f95fa93e56f0b95b36a4b4d2c6fbe42658f3b
YouLim Oh <63313306+jennifer060697@users.noreply.github.com>
2023-01-22 01:07:23 +0900

    Time: 588 ms (94.61%), Space: 0B (100.00%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -5,3 +5,9 @@ DELETE FROM Person WHERE id NOT IN
 (SELECT * FROM (
     SELECT MIN(id) FROM Person Group By Email) temp
 )
+
+# DELETE p2
+# FROM Person p1
+# JOIN Person p2
+# ON p1.Email = p2.Email
+# where p1.id< p2.id;
